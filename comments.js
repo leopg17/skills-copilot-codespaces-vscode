43ed0e614c2e1e79b2d0e36e128b8a77243df21e
Leonardo <leopg17@gmail.com>
2023-10-13 15:04:28 +0000

    Copilot fourth commit

--- comments.js
+++ comments.js
@@ -1,6 +1,61 @@
 // Create web server for comments
 
-var express = require('express');
+// Import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// Create web server
+const app = express();
+
+// Set port
+const port = 3000;
+
+// Set static folder
+app.use(express.static('public'));
+
+// Set body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Get comments
+app.get('/comments', (req, res) => {
+    fs.readFile('./data/comments.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const comments = JSON.parse(data);
+        res.render('comments', {comments: comments});
+    });
+});
+
+// Post comments
+app.post('/comments', (req, res) => {
+    fs.readFile('./data/comments.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const comments = JSON.parse(data);
+        comments.push(req.body);
+        fs.writeFile('./data/comments.json', JSON.stringify(comments), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.redirect('/comments');
+        });
+    });
+});
+
+// Listen port
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
@@ -15,13 +70,3 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// POST comment
-router.post('/', function(req, res, next) {
-  fs.readFile('./data/comments.json', 'utf8', function(err, data) {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Server Error');
-    }
-    var comments = JSON.parse(data);
-    var newComment = {
-      id: Date.now(),
